3a119936a27fa715674d113e5629a00996717f1b
Sarah Halstead <ehalstead@gmail.com>
2026-05-05 12:28:22 -0700

    added docstrings to utils

--- utils.py
+++ utils.py
@@ -6,33 +6,28 @@ import pandas as pd
 
 
 def create_snowflake_connection():
+    """Creates a connection to snowflake."""
     load_dotenv()
-    conn = None
 
-    try:
-        conn = snowflake.connector.connect(
-            account = os.environ.get('SF_ACCOUNT'),
-            user = os.environ.get('SF_USER'),
-            password = os.environ.get('SF_PASSWORD'),
-            role = os.environ.get('SF_ROLE'),
-            database = os.environ.get('SF_DATABASE'),
-            schema = os.environ.get('SF_SCHEMA'),
-            warehouse = os.environ.get('SF_WAREHOUSE')
-        )
-    except Exception as e:
-        print(f'Error connecting to snowflake: {e}')
+    conn = snowflake.connector.connect(
+        account = os.environ.get('SF_ACCOUNT'),
+        user = os.environ.get('SF_USER'),
+        password = os.environ.get('SF_PASSWORD'),
+        role = os.environ.get('SF_ROLE'),
+        database = os.environ.get('SF_DATABASE'),
+        schema = os.environ.get('SF_SCHEMA'),
+        warehouse = os.environ.get('SF_WAREHOUSE')
+    )
 
     return conn
 
 
 def df_to_snowflake(conn, df, table_name):
-    try:
-        write_pandas(conn, df, table_name, auto_create_table=True, overwrite=True)
-
-    except Exception as e:
-        print(f'Error writing data to snowflake: {e}')
+    """Writes a DataFrame to a Snowflake table, creating or overwriting the table if it exists."""
+    write_pandas(conn, df, table_name, auto_create_table=True, overwrite=True)
 
 
 def load_data(path):
+    """Loads data from a CSV file to a DataFrame, suppresses mixed datatype warning message."""
     df = pd.read_csv(path, low_memory=False)
     return df
