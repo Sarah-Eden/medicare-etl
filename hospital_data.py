0968d100050136b25d7c02b9385fd67bfaed921d
Sarah Halstead <ehalstead@gmail.com>
2026-05-12 17:22:56 -0700

    updated docstrings using extension template

--- hospital_data.py
+++ hospital_data.py
@@ -3,20 +3,59 @@ import numpy as np
 from utils import load_data, standardize_zipcode
 
 
-"""Imports data from a specified CSV file and returns a cleaned DataFrame for upload into Snowflake."""
 def transform_hospital_data(file_path):
+    """Cleans and standardizes hospital data for Snowflake upload.
+
+    Standardizes column names to uppercase with underscores, converts zipcodes to 5
+    digits, converts numeric columns from mixed types to numeric, and converts footnote
+    columns to strings to preserve comma-separated values.
+
+    Args:
+        file_path (str): Relative path to the source CSV file.
+
+    Returns:
+        DataFrame: Cleaned & Standardized hospital data.
+    """
 
     hospital_df = load_data(file_path)
 
-    hospital_df.columns = [col.upper().replace(' ', '_').replace('/', '_') for col in hospital_df.columns]
+    hospital_df.columns = [
+        col.upper().replace(' ', '_').replace('/', '_') for col in hospital_df.columns
+    ]
 
     hospital_df['ZIP_CODE'] = standardize_zipcode(hospital_df['ZIP_CODE'])
 
     hospital_df.replace('Not Available', np.nan, inplace=True)
 
-    num_col = [c for c in hospital_df.columns if "_COUNT" in c]
-    num_col.append('HOSPITAL_OVERALL_RATING')
+    num_col = [
+        'HOSPITAL_OVERALL_RATING',
+        'MORT_GROUP_MEASURE_COUNT',
+        'COUNT_OF_FACILITY_MORT_MEASURES',
+        'COUNT_OF_MORT_MEASURES_BETTER',
+        'COUNT_OF_MORT_MEASURES_NO_DIFFERENT',
+        'COUNT_OF_MORT_MEASURES_WORSE',
+        'SAFETY_GROUP_MEASURE_COUNT',
+        'COUNT_OF_FACILITY_SAFETY_MEASURES',
+        'COUNT_OF_SAFETY_MEASURES_BETTER',
+        'COUNT_OF_SAFETY_MEASURES_NO_DIFFERENT',
+        'COUNT_OF_SAFETY_MEASURES_WORSE',
+        'READM_GROUP_MEASURE_COUNT',
+        'COUNT_OF_FACILITY_READM_MEASURES',
+        'COUNT_OF_READM_MEASURES_BETTER',
+        'COUNT_OF_READM_MEASURES_NO_DIFFERENT',
+        'COUNT_OF_READM_MEASURES_WORSE',
+        'PT_EXP_GROUP_MEASURE_COUNT',
+        'COUNT_OF_FACILITY_PT_EXP_MEASURES',
+        'TE_GROUP_MEASURE_COUNT',
+        'COUNT_OF_FACILITY_TE_MEASURES',
+    ]
 
-    hospital_df[num_col]=hospital_df[num_col].apply(pd.to_numeric)
+    hospital_df[num_col] = hospital_df[num_col].apply(pd.to_numeric)
+
+    footnote_col = [c for c in hospital_df.columns if '_FOOTNOTE' in c]
+
+    for col in footnote_col:
+        not_null = hospital_df[col].notna()
+        hospital_df.loc[not_null, col] = hospital_df.loc[not_null, col].astype(str)
 
     return hospital_df
--- provider_data.py
+++ provider_data.py
@@ -2,48 +2,114 @@ import pandas as pd
 import numpy as np
 from utils import load_data, standardize_zipcode
 
-DEMOGRAPHICS_COLUMNS = ['Rndrng_NPI', 'Rndrng_Prvdr_Last_Org_Name',
-                        'Rndrng_Prvdr_First_Name', 'Rndrng_Prvdr_MI',
-                        'Rndrng_Prvdr_Crdntls', 'Rndrng_Prvdr_Ent_Cd',
-                        'Rndrng_Prvdr_St1', 'Rndrng_Prvdr_St2',
-                        'Rndrng_Prvdr_City', 'Rndrng_Prvdr_State_Abrvtn',
-                        'Rndrng_Prvdr_State_FIPS', 'Rndrng_Prvdr_Zip5', 
-                        'Rndrng_Prvdr_RUCA', 'Rndrng_Prvdr_RUCA_Desc', 
-                        'Rndrng_Prvdr_Cntry', 'Rndrng_Prvdr_Type']
+DEMOGRAPHICS_COLUMNS = [
+    'Rndrng_NPI',
+    'Rndrng_Prvdr_Last_Org_Name',
+    'Rndrng_Prvdr_First_Name',
+    'Rndrng_Prvdr_MI',
+    'Rndrng_Prvdr_Crdntls',
+    'Rndrng_Prvdr_Ent_Cd',
+    'Rndrng_Prvdr_St1',
+    'Rndrng_Prvdr_St2',
+    'Rndrng_Prvdr_City',
+    'Rndrng_Prvdr_State_Abrvtn',
+    'Rndrng_Prvdr_State_FIPS',
+    'Rndrng_Prvdr_Zip5',
+    'Rndrng_Prvdr_RUCA',
+    'Rndrng_Prvdr_RUCA_Desc',
+    'Rndrng_Prvdr_Cntry',
+    'Rndrng_Prvdr_Type',
+]
 
-SERVICES_COLUMNS = ['Rndrng_NPI', 'HCPCS_Cd', 'HCPCS_Desc',
-                    'HCPCS_Drug_Ind', 'Place_Of_Srvc', 'Tot_Benes',
-                    'Tot_Bene_Day_Srvcs', 'Rndrng_Prvdr_Mdcr_Prtcptg_Ind',
-                    'Avg_Sbmtd_Chrg', 'Avg_Mdcr_Alowd_Amt',
-                    'Avg_Mdcr_Pymt_Amt', 'Avg_Mdcr_Stdzd_Amt', 'Tot_Srvcs']
+SERVICES_COLUMNS = [
+    'Rndrng_NPI',
+    'HCPCS_Cd',
+    'HCPCS_Desc',
+    'HCPCS_Drug_Ind',
+    'Place_Of_Srvc',
+    'Tot_Benes',
+    'Tot_Bene_Day_Srvcs',
+    'Rndrng_Prvdr_Mdcr_Prtcptg_Ind',
+    'Avg_Sbmtd_Chrg',
+    'Avg_Mdcr_Alowd_Amt',
+    'Avg_Mdcr_Pymt_Amt',
+    'Avg_Mdcr_Stdzd_Amt',
+    'Tot_Srvcs',
+]
+
 
 def create_demographics_table(df):
+    """Creates a new DataFrame for provider demographic data.
+
+    Copies columns in DEMOGRAPHICS_COLUMNS to new DataFrame, converts zipcode and FIPS
+    columns to strings and pads with zeroes for consistency. Standardizes column names
+    to uppercase with underscores and reduces data to one row per NPI.
+
+    Args:
+        df (DataFrame): Raw data from source CSV file
+
+    Raises:
+        ValueError: Variations in zipcode or FIPS data with the same NPI.
+
+    Returns:
+        demographics_df (DataFrame): Cleaned and standardized provider demographic data. One row per Rndrng_NPI.
+    """
     demographics_df = df[DEMOGRAPHICS_COLUMNS].copy()
 
-    demographics_df['Rndrng_Prvdr_Zip5'] = standardize_zipcode(demographics_df['Rndrng_Prvdr_Zip5'])
+    demographics_df['Rndrng_Prvdr_Zip5'] = standardize_zipcode(
+        demographics_df['Rndrng_Prvdr_Zip5']
+    )
 
     has_fips = demographics_df['Rndrng_Prvdr_State_FIPS'].notna()
-    demographics_df.loc[has_fips, 'Rndrng_Prvdr_State_FIPS'] = demographics_df.loc[has_fips, 'Rndrng_Prvdr_State_FIPS'].astype(str).str.zfill(2)
+    demographics_df.loc[has_fips, 'Rndrng_Prvdr_State_FIPS'] = (
+        demographics_df.loc[has_fips, 'Rndrng_Prvdr_State_FIPS']
+        .astype(str)
+        .str.zfill(2)
+    )
 
     demographics_df.columns = [col.upper() for col in demographics_df.columns]
 
     demographics_df.drop_duplicates(inplace=True)
 
     if demographics_df.shape[0] != df['Rndrng_NPI'].nunique():
-        raise ValueError(f'Inconsistencies within zipcode or FIPS data. Please verify output.')
-    
+        raise ValueError(
+            f'Variations in zipcode or FIPS data with the same NPI. Please verify data.'
+        )
+
     return demographics_df
- 
+
 
 def create_services_table(df):
+    """Creates a new DataFrame for provider service data.
+
+    Copies columns in SERVICES_COLUMNS to new DataFrame and standardizes column names
+    to uppercase with underscores.
+
+    Args:
+        df (DataFrame): Raw data from source CSV file
+
+    Returns:
+        services_df (DataFrame): Standardized provider service data. One row per NPI and HCPCS code combination.
+    """
     services_df = df[SERVICES_COLUMNS].copy()
 
     services_df.columns = [col.upper() for col in services_df.columns]
 
-    return services_df 
+    return services_df
 
 
 def transform_provider_data(file_path):
+    """Coordinates the cleaning and transformation of the provided data for Snowflake upload.
+
+    Splits the data from the provided CSV file into demographic and service data tables.
+
+    Args:
+        file_path (str): Relative path to the source CSV file.
+
+    Returns:
+        demographic_df (DataFrame): Standardized provider demographic data. One row per RNDRNG_NPI.
+        services_df aut(DataFrame): Standardized provider service data. One row per NPI and HCPCS code combination.
+    """
 
     provider_df = load_data(file_path)
 
@@ -51,4 +117,3 @@ def transform_provider_data(file_path):
     services_df = create_services_table(provider_df)
 
     return demographics_df, services_df
-
